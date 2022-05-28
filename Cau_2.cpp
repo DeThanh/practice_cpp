a4f9e0ffa94b661b00c9a06f4f78eb6e7a900bfb
DeThanh <87396977+DeThanh@users.noreply.github.com>
2022-05-28 22:32:11 +0700

    Add files via upload

---
+++ Cau_2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+const int maxE = (int)1e6 + 2;
+int a , b , k;
+int E[maxE + 5];
+void eratos()
+{
+    E[1] = 0;
+    for (int i = 2; i <= maxE; i++) E[i] = 1;
+    for (int i = 2; i * i <= maxE; i++)
+        if (E[i] == 1)
+        for (int j = 2*i; j <= maxE; j += i) E[j] = 0;
+    //for (int i = 2; i <= 100; i++) cout << E[i] << " "; cout << "\n";
+    for (int i = 2; i <= maxE; i++) E[i] = E[i - 1] + E[i]; // prefix_sum
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+ 	    cin.tie(0);
+    eratos();
+    cin >> k;
+    while (k--)
+    {
+        cin >> a >> b;
+        cout << E[b] - E[a - 1] << "\n";
+    }
+    return 0;
+}
